2480cc7001f688786281328e748f5b41800b137b
Aurore Maureaud <aurore.aqua@gmail.com>
2023-11-21 16:15:19 +0100

    create table 1 to update easily

---
+++ data_descriptor_figures/make_tables.R
@@ -0,0 +1,26 @@
+#######################################################
+#### Coding: Aurore Maureaud, November 2023
+#### Public survey metadata
+#### Figures for data paper
+#######################################################
+rm(list = ls())
+
+# load libraries
+library(tidyverse)
+
+load("outputs/Compiled_data/FishGlob_public_std_clean.RData")
+
+table_1 <- data %>% 
+  group_by(survey) %>% 
+  mutate(min_year = min(year),
+         max_year = max(year),
+         number_of_years = length(unique(year)),
+         month = as.numeric(month),
+         months = paste(sort(unique(month)), collapse = ", "),
+         number_of_hauls = length(unique(haul_id))) %>% 
+  dplyr::select(survey, min_year, max_year, number_of_years,
+                months, number_of_hauls) %>% 
+  distinct()
+
+write.csv(table_1, file = "data_descriptor_figures/table_1.csv", 
+          row.names = F)
